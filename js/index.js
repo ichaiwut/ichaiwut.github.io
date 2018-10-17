e401e0edecde2678af6a8aabe860744ef5928176
TingSter <ting@buzzwoo.de>
2018-10-17 15:06:26 +0700

    Improve the perfomance

--- js/index.js
+++ js/index.js
@@ -2,6 +2,149 @@ import Post from './modules/Post';
 
 document.onreadystatechange = () => {
   if (document.readyState === "complete") {
+
+    // Code form the original theme
+    var sect = $(window.location.hash);
+
+    function switchSection(section) {
+      if (section.length == 1) {
+        if (!section.hasClass('active')) {
+          $('body').addClass('section-animate');
+          setTimeout(function () {
+            $('body').removeClass('section-animate')
+            $('.section.active').removeClass('active');
+            section.addClass('active');
+          }, 400);
+        }
+        if ($('body').hasClass('show-menu')) {
+          $('body').removeClass('show-menu');
+          trigger.click();
+        }
+        if (section.hasClass('remain-col')) {
+          $('body').removeClass('no-col');
+        } else {
+          $('body').addClass('no-col');
+        }
+      }
+    }
+
+    $('.skill').each(function () {
+      var $this = $(this),
+        percent = $this.data('percent');
+      $this.append("<span class='percent' >" + percent + "%</span><div class='skill-bar' ><div style='width:" + percent + "%;' ></div></div>");
+    });
+
+    $(window).on('load', function () {
+      switchSection(sect);
+      $('body').addClass('loaded');
+    });
+
+    $('.menu-items a, .section-link').on('click', function (e) {
+      var $this = $(this),
+        section = $($this.attr('href'));
+      switchSection(section);
+    });
+
+    var beginAC = 80,
+      endAC = 320,
+      beginB = 80,
+      endB = 320;
+
+    function inAC(s) {
+      s.draw('80% - 240', '80%', 0.3, {
+        delay: 0.1,
+        callback: function () {
+          inAC2(s)
+        }
+      });
+    }
+
+    function inAC2(s) {
+      s.draw('100% - 545', '100% - 305', 0.6, {
+        easing: ease.ease('elastic-out', 1, 0.3)
+      });
+    }
+
+    function inB(s) {
+      s.draw(beginB - 60, endB + 60, 0.1, {
+        callback: function () {
+          inB2(s)
+        }
+      });
+    }
+
+    function inB2(s) {
+      s.draw(beginB + 120, endB - 120, 0.3, {
+        easing: ease.ease('bounce-out', 1, 0.3)
+      });
+    }
+
+    /* Out animations (to burger icon) */
+    function outAC(s) {
+      s.draw('90% - 240', '90%', 0.1, {
+        easing: ease.ease('elastic-in', 1, 0.3),
+        callback: function () {
+          outAC2(s)
+        }
+      });
+    }
+
+    function outAC2(s) {
+      s.draw('20% - 240', '20%', 0.3, {
+        callback: function () {
+          outAC3(s)
+        }
+      });
+    }
+
+    function outAC3(s) {
+      s.draw(beginAC, endAC, 0.7, {
+        easing: ease.ease('elastic-out', 1, 0.3)
+      });
+    }
+
+    function outB(s) {
+      s.draw(beginB, endB, 0.7, {
+        delay: 0.1,
+        easing: ease.ease('elastic-out', 2, 0.4)
+      });
+    }
+
+    var pathA = document.getElementById('pathA'),
+		pathB = document.getElementById('pathB'),
+		pathC = document.getElementById('pathC'),
+		segmentA = new Segment(pathA, beginAC, endAC),
+		segmentB = new Segment(pathB, beginB, endB),
+		segmentC = new Segment(pathC, beginAC, endAC),
+		trigger = document.getElementById('menu-icon-trigger'),
+		toCloseIcon = true,
+		wrapper = document.getElementById('menu-icon-wrapper');
+	wrapper.style.visibility = 'visible';
+	trigger.onclick = function() {
+		if (toCloseIcon) {
+			inAC(segmentA);
+			inB(segmentB);
+			inAC(segmentC);
+			$('body').addClass('show-menu');
+		} else {
+			outAC(segmentA);
+			outB(segmentB);
+			outAC(segmentC);
+			$('body').removeClass('show-menu');
+		}
+		toCloseIcon = !toCloseIcon;
+	};
+
+	/* Scale functions */
+	function addScale(m) {
+		m.className = 'menu-icon-wrapper scaled';
+	}
+	function removeScale(m) {
+		m.className = 'menu-icon-wrapper';
+  }
+
+  // New code
+
     const post = new Post();
 
     let body = document.querySelector("body");
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require("path");
 var webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractCSS = new ExtractTextPlugin('styles.min.css');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const ConcatPlugin = require("webpack-concat-plugin");
 
 module.exports = {
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
   mode: "production",
-  entry: ["./js/index.js"],
+  entry: ["./js/index.js", "./css/style.scss"],
   output: {
     path: path.resolve(__dirname, "dist/"),
     filename: "bundle.js"
@@ -19,7 +35,44 @@ module.exports = {
             presets: ["babel-preset-env"]
           }
         }
+      },
+      {
+        test: [/\.scss$/, /\.sass$/],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "resolve-url-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              outputStyle: "compressed"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000"
       }
     ]
-  }
+  },
+  plugins: [
+    extractCSS,
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new ConcatPlugin({
+      name: 'libs',
+      fileName: '[name].js',
+      filesToConcat: [
+        'jquery',
+        './js/ease.min.js',
+        './js/segment.min.js'
+      ],
+      attributes: {
+        async: true
+      }
+    })
+  ]
 };
