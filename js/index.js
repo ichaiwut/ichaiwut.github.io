3dafbefad3dcac566fe8869014d9224fb53f8ee2
TingSter <ting@buzzwoo.de>
2018-10-18 11:13:44 +0700

    add service worker

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 import Post from './modules/Post';
 
+// Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 document.onreadystatechange = () => {
   if (document.readyState === "complete") {
+
 
     // Code form the original theme
     var sect = $(window.location.hash);
